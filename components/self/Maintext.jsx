933446ea06cd8452528f0fe515151f3d41f30bee
Amanfromearth <anurag3b@gmail.com>
2024-07-13 11:32:34 +0530

    minor changes

--- components/self/Maintext.jsx
+++ components/self/Maintext.jsx
@@ -1,15 +1,11 @@
 import { memo } from 'react';
 import { FlipWords } from '../ui/flipwords';
-
-const WORDS = ["Hello, I'm Anurag Bevinal"];
-
-const MainText = memo(() => (
-  <section className="w-full p-5 md:p-10 mt-8 bg-transparent">
-    <div className="w-full lg:w-3/4 flex flex-col gap-3 text-black dark:text-white">
-      <h1 className="text-xl md:text-4xl font-semibold">
-        <FlipWords words={WORDS} />
-      </h1>
-      <p className="text-neutral-500 text-lg md:text-3xl">
+const words = ["Hello, I'm Anurag Bevinal"]
+const Maintext = memo(() => (
+  <section  className="w-full inline-flex p-5 md:p-10 mt-8 bg-transparent">
+    <div className="w-full lg:w-[75%] flex flex-col gap-2 leading-7 text-black dark:text-white text-lg md:text-3xl font-normal">
+      <h1 className='text-xl md:text-4xl font-semibold'><FlipWords words={words} /></h1>
+      <p className="text-neutral-500\">
         Full-Stack Developer
       </p>
       <div className="flex items-center gap-2">
@@ -21,16 +17,13 @@ const MainText = memo(() => (
           Available for full-time role
         </p>
       </div>
-      <p className="text-lg md:text-3xl leading-7">
-        In the past two years, I co-founded Zlock Technologies and developed web startups, 
-        gaining diverse skills. I focus on creating scalable, user-friendly applications 
-        to solve real-world challenges, innovating and adapting in dynamic environments 
-        to deliver impactful solutions.
+      <p>
+      In the past two years, I co-founded Zlock Technologies and developed web startups, gaining diverse skills. I focus on creating scalable, user-friendly applications to solve real-world challenges, innovating and adapting in dynamic environments to deliver impactful solutions.
       </p>
     </div>
   </section>
 ));
 
-MainText.displayName = 'MainText';
+Maintext.displayName = 'Maintext';
 
-export default MainText;
+export default Maintext;
