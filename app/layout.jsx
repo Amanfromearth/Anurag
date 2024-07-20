fb9a4d70734c9b102f9009bc240168e02a59a273
Amanfromearth <anurag3b@gmail.com>
2024-07-20 17:29:54 +0530

    analytics

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,5 @@
 import { DM_Sans } from "next/font/google";
-import Head from "next/head";
+import mixpanel from 'mixpanel-browser';
 import "./globals.css";
 import Nav from "@/components/self/Nav";
 import Footer from "@/components/self/Footer";
@@ -30,20 +30,11 @@ export const metadata = {
 };
 
 export default function RootLayout({ children }) {
+  mixpanel.init('012fb6bb89fb114cdd3ec8c4271829f1', {debug: true, track_pageview: true, persistence: 'localStorage'});
   return (
     <html lang="en" className="dark">
       <head>
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-E0FKVBGVHZ"></script>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', 'G-E0FKVBGVHZ');
-            `,
-          }}
-        />
+       
       </head>
       <body className={`${dmsans.className} relative`}>
         <SoundProvider>
