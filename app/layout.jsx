9ad1d9769c7ae0e6a22a5dd677fac4a145b0a335
Amanfromearth <anurag3b@gmail.com>
2024-07-20 17:13:43 +0530

    analytics

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,11 +1,11 @@
 import { DM_Sans } from "next/font/google";
+import Head from "next/head";
 import "./globals.css";
 import Nav from "@/components/self/Nav";
 import Footer from "@/components/self/Footer";
 import DrawerComponent from "@/components/self/DrawerComponent";
 import Cta from "@/components/self/cta";
 import { SoundProvider } from "@/components/hooks/SoundContext";
-import { GoogleAnalytics } from '@next/third-parties/google'
 
 const dmsans = DM_Sans({ subsets: ["latin"] });
 
@@ -26,28 +26,39 @@ export const metadata = {
     title: "Anurag's Portfolio",
     description: "Full-stack developer portfolio showcasing projects and skills",
     image: "/header.webp",
-  }
+  },
 };
 
 export default function RootLayout({ children }) {
   return (
-
     <html lang="en" className="dark">
+      <head>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-E0FKVBGVHZ"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-E0FKVBGVHZ');
+            `,
+          }}
+        />
+      </head>
       <body className={`${dmsans.className} relative`}>
-      <SoundProvider>
-        <div className="fixed inset-0 hidden md:block h-screen -z-10 bg-[radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]" />
-        <div id="scrollable-content" className="h-screen overflow-y-auto">
-          <main className="relative text-white mx-auto w-full lg:w-[940px] lg:mt-28 bg-dark-blue rounded border border-neutral-700">
-            <Nav />
-            {children}
-            <Cta />
-            <Footer />
-          </main>
-        </div>
-        <DrawerComponent />
+        <SoundProvider>
+          <div className="fixed inset-0 hidden md:block h-screen -z-10 bg-[radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]" />
+          <div id="scrollable-content" className="h-screen overflow-y-auto">
+            <main className="relative text-white mx-auto w-full lg:w-[940px] lg:mt-28 bg-dark-blue rounded border border-neutral-700">
+              <Nav />
+              {children}
+              <Cta />
+              <Footer />
+            </main>
+          </div>
+          <DrawerComponent />
         </SoundProvider>
       </body>
-      <GoogleAnalytics gaId="G-E0FKVBGVHZ" />
     </html>
   );
 }
